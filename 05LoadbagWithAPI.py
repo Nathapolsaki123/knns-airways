117ae244cb444e4bf286d2af5f264dd6a8809444
Peerapas Intrapong <peerapasi87@gmail.com>
2026-03-09 12:24:29 +0700

    AddPaymentMethod

--- 05LoadbagWithAPI.py
+++ 05LoadbagWithAPI.py
@@ -4,20 +4,25 @@ from abc import ABC,abstractmethod
 
 app = FastAPI()
 
-class Payment(ABC):
+class Payment_Method(ABC):
 
     @abstractmethod
     def pay(self):
         pass
 
 class Booking:
-    def __init__(self, pnr: str, seat_class :str):
+    def __init__(self, pnr: str, passenger,flight_in, seat_type :str):
         self.__pnr = pnr
-        self.__seat_class = seat_class
+        self.__passenger = passenger
+        self.__flight_instance = flight_in
+        self.__seat_type = seat_type
+        self.__booking_status = None
+        self.__payment_status = None
+        self.__booking_date = None
         self.__transaction = None
     
-    def get_seat_class(self):
-        return self.__seat_class
+    def get_seat_type(self):
+        return self.__seat_type
     
     def get_pnr(self):
         return self.__pnr
@@ -29,30 +34,33 @@ class Booking:
         self.__transaction = transaction
 
 class Transaction:
-    def __init__(self,name,amount):
+    def __init__(self,name,amount,payment_type):
         self.__name = name
         self.__amount = amount
-        self.__subtransaction_list:list = []
+        self.__subtrans_list:list = []
+        self.__payment_type = payment_type
 
     def add_subtransaction(self,subtransaction):
-        self.__subtransaction_list.append(subtransaction)
+        self.__subtrans_list.append(subtransaction)
 
     def get_all_subtransaction(self):
         sub_list = [{self.__name:self.__amount}]
-        for sub in self.__subtransaction_list:
+        for sub in self.__subtrans_list:
             sub_list.append(sub.get_data())
         return sub_list
 
 
 class Subtransaction:
-    def __init__(self,name,amount):
+    def __init__(self,name,amount,payment_type):
         self.__name = name
         self.__amount = amount
+        self.__payment_type = payment_type
 
     def get_data(self):
-        return {self.__name:self.__amount}
+        return {self.__name:self.__amount,"Payment_type":self.__payment_type.get_name()}
 
-class Card(Payment):
+
+class Card:
     def __init__(self,card_no:str,amount:float):
         self.__card_no = card_no
         self.__amount = amount
@@ -67,11 +75,25 @@ class Card(Payment):
             raise HTTPException(status_code=404,detail="Not enought money")
         self.__amount-=fee
 
+
+class Pay_By_Card(Payment_Method):
+
+    def pay(self,card:Card,fee):
+        card.pay(fee)
+
+    def get_name(self):
+        return "Card"
+
 class Passenger:
-    def __init__(self, first_name: str,luggage_weight: int):
-        self.__first_name = first_name
+    def __init__(self, passenger_id:str, name: str,email,luggage_weight: int):
+        self.__passenger_id = passenger_id
+        self.__name = name
+        self.__email = email
         self.__booking_list:list = []
-        self.__tier = "Gold"
+        self.__total_refunded:int = 0
+        self.__is_blacklisted:bool = False
+        self.__blacklist_time = None
+        self.__notification:list = []
         if(luggage_weight<0):
             raise HTTPException(status_code=404,detail="Luggage weight must be more than 0")
         self.__luggage_weight = luggage_weight
@@ -85,7 +107,7 @@ class Passenger:
         self.__luggage_weight = weight
     
     def get_name(self):
-        return self.__first_name
+        return self.__name
     
     def get_tier(self):
         return self.__tier
@@ -102,12 +124,134 @@ class Passenger:
     def add_card(self,card:Card):
         self.__card = card
 
+
+class Member(Passenger):
+
+    def __init__(self, passenger_id, name, email, luggage_weight):
+        super().__init__(passenger_id, name, email, luggage_weight)
+        self.__point = 0
+
+    def add_point(self,point):
+        self.__point+=point
+
+
+class Silver(Member):
+
+    DISCOUNT = 0.05
+    EXTRA_WEIGHT = 5
+    ANNUAL_FEE = 200
+
+    def get_weight(self) -> int:
+        return super().get_weight()
+    def set_weight(self,weight):
+        if(weight<0):
+            raise HTTPException(status_code=404,detail="Luggage weight must be more than 0")
+        super().set_weight(weight)
+    
+    def get_name(self):
+        return super().get_name()
+    
+    def get_tier(self):
+        return "Gold"
+    
+    def get_booking_list(self):
+        return super().get_booking_list()
+    
+    def get_card(self):
+        return super().get_card()
+    
+    def add_booking(self,booking:Booking):
+        super().add_booking(booking)
+
+    def add_card(self,card:Card):
+        super().add_card(card)
+
+    def add_point(self,point):
+        super().add_point(point)
+
+
+
+class Gold(Member):
+
+    DISCOUNT = 0.1
+    EXTRA_WEIGHT = 10
+    ANNUAL_FEE = 300
+
+    def get_weight(self) -> int:
+        return super().get_weight()
+    def set_weight(self,weight):
+        if(weight<0):
+            raise HTTPException(status_code=404,detail="Luggage weight must be more than 0")
+        super().set_weight(weight)
+    
+    def get_name(self):
+        return super().get_name()
+    
+    def get_tier(self):
+        return "Gold"
+    
+    def get_booking_list(self):
+        return super().get_booking_list()
+    
+    def get_card(self):
+        return super().get_card()
+    
+    def add_booking(self,booking:Booking):
+        super().add_booking(booking)
+
+    def add_card(self,card:Card):
+        super().add_card(card)
+
+    def add_point(self,point):
+        super().add_point(point)
+
+
+class Platinum(Member):
+
+    DISCOUNT = 0.15
+    EXTRA_WEIGHT = 20
+    ANNUAL_FEE = 500
+
+    def get_weight(self) -> int:
+        return super().get_weight()
+    def set_weight(self,weight):
+        if(weight<0):
+            raise HTTPException(status_code=404,detail="Luggage weight must be more than 0")
+        super().set_weight(weight)
+    
+    def get_name(self):
+        return super().get_name()
+    
+    def get_tier(self):
+        return "Gold"
+    
+    def get_booking_list(self):
+        return super().get_booking_list()
+    
+    def get_card(self):
+        return super().get_card()
+    
+    def add_booking(self,booking:Booking):
+        super().add_booking(booking)
+
+    def add_card(self,card:Card):
+        super().add_card(card)
+
+    def add_point(self,point):
+        super().add_point(point)
+
+
 class Airline:
     ECONOMYCLASS_LIMIT_WEIGHT = 15
     BUSSINESSCLASS_LIMIT_WEIGHT = 30
-    def __init__(self):
-        self.__extra_fee_per_kg = 250  # บาท / กิโล
+    EXTRA_FEE_PER_KG = 300
+    def __init__(self,name):
+        self.__name = name
         self.__passenger_list:list = []
+        self.__booking_list:list = []
+        self.__airplane_list:list = []
+        self.__flight_list:list = []
+        self.__blacklist:list = []
 
     def add_passenger(self,passenger:Passenger):
         self.__passenger_list.append(passenger)
@@ -121,13 +265,11 @@ class Airline:
     
     def get_weight_limit(self,pnr:str)->int:
         current_data = self.get_data_by_pnr(pnr)
-        seat_class = current_data[1].get_seat_class()
-        passenger_tier = current_data[0].get_tier()
+        seat_class = current_data[1].get_seat_type()
+        extra_weight = current_data[0].EXTRA_WEIGHT
         weight_limit_before_tier = self.ECONOMYCLASS_LIMIT_WEIGHT if(seat_class == "Economy") else self.BUSSINESSCLASS_LIMIT_WEIGHT
 
-        if(passenger_tier == "Silver"): return weight_limit_before_tier
-        if(passenger_tier == "Gold"): return weight_limit_before_tier*1.5
-        if(passenger_tier == "Platinum"): return weight_limit_before_tier*2
+        return weight_limit_before_tier+extra_weight
 
 
     def verify_weight(self,weight_limit:int,passenger:Passenger) -> bool:
@@ -141,6 +283,7 @@ class Airline:
         passenger = self.get_data_by_pnr(pnr)[0]
         booking = self.get_data_by_pnr(pnr)[1]
         card = passenger.get_card()
+        discount = passenger.DISCOUNT
 
         # verify luggage weight
         if self.verify_weight(weight_limit,passenger):
@@ -152,38 +295,46 @@ class Airline:
 
         # calculate extra fee
         extra_weight = passenger.get_weight() -weight_limit
-        extra_fee = self.__calculate_extra_weight_fee(extra_weight)
+        extra_fee_before_discount = self.__calculate_extra_weight_fee(extra_weight)
+        extra_fee = extra_fee_before_discount*(1-discount)
 
         # payment
-        payment_result = self.payment_process(card,extra_fee)
+        payment_result = self.payment_process("Card",extra_fee,passenger)  
         if not payment_result:
             return "Error: Extra baggage payment failed"
         
         # create_subtransaction
-        subtransaction = Subtransaction("Load_luggage_fee",extra_fee)
         transaction = booking.get_transaction()
-        transaction.add_subtransaction(subtransaction)
+        transaction.add_subtransaction(payment_result)
 
         return {"name":passenger.get_name(),
            "luggage_weight":passenger.get_weight(),
            "weight_limit":weight_limit,
-        "message":f"Luggage loaded (Extra Fee :{extra_weight} X {self.__extra_fee_per_kg} = {extra_fee})",
+        "message":f"Luggage loaded (Extra Fee :[{extra_weight} X {self.EXTRA_FEE_PER_KG}] - {passenger.DISCOUNT*100}% = {extra_fee})",
         "transaction":booking.get_transaction().get_all_subtransaction()
         }
     
     def __calculate_extra_weight_fee(self, extra_weight: int) -> int:
-        return extra_weight * self.__extra_fee_per_kg
+        return extra_weight * self.EXTRA_FEE_PER_KG
     
-    def payment_process(self,channel,fee:int):
-        channel.pay(fee)
-        return True
+    def payment_process(self,channel,fee:int,passenger:Passenger) -> Subtransaction:
 
+        try:
+            if(channel == "Card"):
+                temp_payment_channel = Pay_By_Card()
+                card = passenger.get_card()
+                temp_payment_channel.pay(card,fee)
+                subtransaction = Subtransaction("Load_luggage_fee",fee,temp_payment_channel)
+                return subtransaction
+        except:
+            raise HTTPException(status_code=404,detail="Payment Failed")
 
-airline = Airline()
-booking = Booking(pnr="ABC123",seat_class="Business")
-passenger = Passenger(first_name="John", luggage_weight=50)
+temp_payment = Pay_By_Card()
+airline = Airline("KNNS-Airways")
+passenger = Platinum("P1234","John","abcd@gmail.com", luggage_weight=60)
+booking = Booking("ABC123",passenger,"ABC","Business")
 card = Card("1234-5678-9101-1121",10000)
-transaction = Transaction("Booking",10000)
+transaction = Transaction("Booking",10000,temp_payment)
 
 passenger.add_booking(booking)
 passenger.add_card(card)
